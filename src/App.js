8c7bcefbea36bcc7b5b6990ba6f20e4f52455e63
juliaheller <julia.natsuko@gmail.com>
2022-01-04 17:36:12 +0100

    Add sceletons for tab container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonContainer from './components/person/PersonContainer';
+import ListContainer from './components/list/ListContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonContainer />
+      <ListContainer />
     </div>
   );
 }
